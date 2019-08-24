3b9edf033bb785c48d579caee3b8f584d01d2e22
rzmorales <robertzaratemorales@pursuit.org>
2019-08-24 15:18:38 -0400

    implement Presenter

---
+++ app/src/main/java/com/example/mvparchitecturepractice/FizzPresenter.java
@@ -0,0 +1,10 @@
+package com.example.mvparchitecturepractice;
+
+public class FizzPresenter implements FizzContract.PresenterContract{
+    private FizzContract.ViewContract viewContract;
+
+    @Override
+    public void showFizzOrBuzz(String fizzOrBuzz) {
+
+    }
+}
